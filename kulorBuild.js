e6f87d4809aecd884ed2d42ae517d6caefb76235
bo.cheng <chen8114@sina.com>
2015-06-22 15:23:17 +0800

    update fonts copy

--- kulorBuild.js
+++ kulorBuild.js
@@ -6,7 +6,7 @@ module.exports  = function( bower , grunt , tool , log , callback ) {
         .install( [ "bootstrap" ] , { save : true } )
         .on( "end" , function( installed ){
             tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/css" ) , path.resolve( self.cwd , "src/css/lib" ) );
-            tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/fonts" ) , path.resolve( self.cwd , "src/css/lib" ) );
+            tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/fonts" ) , path.resolve( self.cwd , "src/css/fonts" ) );
             tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/js" ) , path.resolve( self.cwd , "src/js/bootstrap" ) );
             log( "bootstrap init success" );
             callback();
