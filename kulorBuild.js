838ec0a16992c89393c7ab24e4e4c45bcd6270ef
bo.cheng <chen8114@sina.com>
2015-06-22 15:17:04 +0800

    fixed path

--- kulorBuild.js
+++ kulorBuild.js
@@ -1,12 +1,13 @@
+var path    = require( "path" );
 module.exports  = function( bower , grunt , tool , log , callback ) {
     var self    = this;
     log( "start load bootstrap" );
     bower.commands
         .install( [ "bootstrap" ] , { save : true } )
         .on( "end" , function( installed ){
-            tool.file.copy( self.bowerDir + "bootstrap/dist/css" , self.cwd + "src/css/lib" );
-            tool.file.copy( self.bowerDir + "bootstrap/dist/font" , self.cwd + "src/css/lib" );
-            tool.file.copy( self.bowerDir + "bootstrap/dist/js" , self.cwd + "src/js/bootstrap" );
+            tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/css" ) , path.resolve( self.cwd , "src/css/lib" ) );
+            tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/fonts" ) , path.resolve( self.cwd , "src/css/lib" ) );
+            tool.file.copy( path.resolve( self.bowerDir , "bootstrap/dist/js" ) , path.resolve( self.cwd , "src/js/bootstrap" ) );
             log( "bootstrap init success" );
             callback();
         } );
